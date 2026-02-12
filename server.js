780ebdfe00d914c40e8fb6cd5a60025cf1d31dd7
poutagamerrr <omarbenmaaouia16@gmail.com>
2026-02-12 12:23:16 +0100

    cors origin 2 urls

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL_1 || process.env.FRONTEND_URL_2 || 'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json());
